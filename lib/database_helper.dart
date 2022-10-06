e56efbbff99ed630879cbad498448c8aae8a1fdb
Eduardo Vitor <eduardovitor730@gmail.com>
2022-10-06 09:55:08 -0300

    Adicionando a tabela de notificacao

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -13,7 +13,7 @@ class DatabaseHelper {
 
   _initDB() async {
     String databasePath = await getDatabasesPath();
-    String path = join(databasePath, 'teste.db');
+    String path = join(databasePath, 'teste2.db');
     //  print('> Path database: $path');
 
     var database = await openDatabase(
@@ -30,9 +30,28 @@ class DatabaseHelper {
         'CREATE TABLE usuario(id int auto_increment primary key, nome varchar(50) NOT NULL, email varchar(30) NOT NULL, senha VARCHAR(16) NOT NULL);';
     await db.execute(sql);
 
+    sql =
+        'CREATE TABLE notificacoes(id int auto_increment primary key, texto varchar(100));';
+    await db.execute(sql);
+
     //Inserções
     sql =
         "INSERT INTO usuario (nome, email, senha) VALUES ('admin','admin@gmail.com','123');";
+    await db.execute(sql);
+
+    sql =
+        "INSERT INTO notificacoes (texto) VALUES ('Genildo curtiu sua foto');";
+    await db.execute(sql);
+
+    sql = "INSERT INTO notificacoes (texto) VALUES ('Luana curtiu sua foto');";
+    await db.execute(sql);
+
+    sql =
+        "INSERT INTO notificacoes (texto) VALUES ('Fernando te mandou uma mensagem');";
+    await db.execute(sql);
+
+    sql =
+        "INSERT INTO notificacoes (texto) VALUES ('IFAL fez uma publicação');";
     await db.execute(sql);
   }
 }
